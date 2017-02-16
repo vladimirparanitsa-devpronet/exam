c82ea76c2b5ac0e392a96370d0c5cbcddbf2a2cf
Vladimir Paranitsa <vladimir.paranitsa@dev-pro.net>
2017-02-16 16:01:33 +0200

    download page data recursivly

--- parse/PageParser.js
+++ parse/PageParser.js
@@ -4,23 +4,26 @@ const fetch = require('node-fetch');
 const urlNode = require('url');
 const config = require('../config');
 
+const parsedLinks = [];
+const linkToParse = [];
+let level = 0;
+
 class PageParser {
   constructor(req, res) {
     this.request = req;
     this.response = res;
-    this.query = this.request.query;
+    this.url = req.query.url;
+    this.element = req.query.element;
+    this.level = req.query.level;
     this.links = [];
-    this.level = this.query.level || config.app.level;
+    this.level = this.level || config.app.level;
     this.depth = 0;
     this.parsedLinks = [];
-
-    this.links.push(this.query.url);
   }
 
   getPageHtml(url) {
     return fetch(url)
       .then(res => res.text())
-      .then(text => text)
       .catch((error) => {
         console.log(error);
         return false;
@@ -39,11 +42,11 @@ class PageParser {
 
   grabLinks() {
     let href = '';
-    this.$(`a[href^='${this.query.url}']`).map((index, tag) => {
+    this.$(`a[href^='${this.url}']`).map((index, tag) => {
       href = this.prepareLinks(this.$(tag).attr('href'));
 
-      if (this.links.indexOf(href) === -1) {
-        this.links.push(href);
+      if (parsedLinks.indexOf(href) === -1) {
+        linkToParse.push(href);
       }
     });
 
@@ -54,6 +57,32 @@ class PageParser {
     const urlParts = urlNode.parse(href);
 
     return `${urlParts.protocol}//${urlParts.host}${urlParts.path}`;
+  }
+
+  grabPageData(url = this.url, element, res) {
+    return this
+      .getPageHtml(url)
+      .then((result) => {
+        const pageElements = this.grabPageElements(result, element);
+        const pageLinks = this.grabLinks(result);
+  
+        parsedLinks.push(url);
+        linkToParse.push(...pageLinks);
+
+        console.log(linkToParse, parsedLinks);
+
+        if (parsedLinks.length === linkToParse.length || level >= parsedLinks.length) {
+          console.log('Done');
+          res.end(JSON.stringify([pageElements, parsedLinks]));
+          return true;
+        }
+
+        this.grabPageData(linkToParse[linkToParse.indexOf(url) + 1]);
+      })
+      .catch((error) => {
+        console.log(error);
+        return;
+      });
   }
 }
 
