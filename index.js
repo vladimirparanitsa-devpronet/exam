6aa257cf545aa7264053e7aac734a03846483299
Alexander Korzhykov <fractalawareness@gmail.com>
2017-03-04 19:41:03 +0200

    useful comments

--- index.js
+++ index.js
@@ -10,26 +10,27 @@ const app = express();
 const port = config.server.port || 3000;
 const db = new Db();
 
+// better place your routes in a separate routes files
 app.get('/api/search', (req, res) => {
   res.setHeader('Content-type', 'application/json');
 
   const pageParser = new Parser(req, res);
-
-  db.getData(pageParser.key, (result) => {
-    if (result) {
-      return res.end(result);
-    }
-
-    pageParser.grabPageData();
-  });
+  // better place your app logic in separate controllers files
+  db.getData(pageParser.key)
+    .then((result) => {
+      if (result) {
+        console.log('resuylt', result);
+        return res.end(result);
+      }
+      pageParser.grabPageData();
+    });
 });
 
 app.get('/api/search/list', (req, res) => {
   res.setHeader('Content-type', 'application/json');
-
-  db.getRecord('history', (result) => {
-    res.end(result);
-  });
+  db.getHistory()
+    .then(result => res.json(result))
+    .catch(err => res.status(err.statusCode || 500).json(err.message));
 });
 
 app.delete('/api/search', (req, res) => {
@@ -37,16 +38,18 @@ app.delete('/api/search', (req, res) => {
 
   db.deleteRecord(pageParser.key, (error, result) => {
     if (error) {
-      res.end({ error: 'Please try again later' });
-      console.log(error);
+      console.error(error);
+      // the code execution will continue if you dont return here
+      return res.end({ error: 'Please try again later' });
     }
     res.setHeader('Content-type', 'application/json');
 
-    if (! result) {
-      res.status(404).end(JSON.stringify({ status: 'Error', message: 'Not Found' }));
+    if (!result) {
+      // the code execution will continue if you dont return here
+      return res.status(404).end(JSON.stringify({ status: 'Error', message: 'Not Found' }));
     }
-
-    res.end(JSON.stringify({ status: 'Removed', message: 'OK' }));
+    // you can use res.json method, it converts objects to json and sends 200 status
+    res.json({ status: 'Removed', message: 'OK' });
   });
 });
 
@@ -57,6 +60,7 @@ app.use('/v1', subpath);
 swagger.setAppHandler(subpath);
 app.use(express.static('dist'));
 
+// better place it to config
 swagger.setApiInfo({
   title: 'Scraper API',
   description: 'API scrap',
